422066dd819ecb2f09324aa64a16fae6755293d8
Yuwei <42990174+Yuwei-Chang@users.noreply.github.com>
2019-02-13 17:14:18 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import ArticleList from './ArticleList/ArticleList';
+import articles from './data/articles.json';
+import ArticleTable from './ArticleTable';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ArticleList articles={articles} />
+        <ArticleTable articles={articles} />
+      </div>
+    );
+  }
+}
+
+export default App;
