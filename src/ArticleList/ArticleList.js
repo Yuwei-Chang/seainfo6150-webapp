b7bf15d03b8f81d64413feb4e9f6442b14763505
Yuwei <42990174+Yuwei-Chang@users.noreply.github.com>
2019-03-06 16:28:13 -0800

    Add files via upload

--- src/ArticleList/ArticleList.js
+++ src/ArticleList/ArticleList.js
@@ -2,13 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ArticleListItem from './ArticleListItem';
 import styles from './ArticleList.module.css';
-
-
+import Header from './Header.js';
+import Footer from './Footer.js';
 
 const ArticleList = ({ articles }) => (
     <section className={styles.background}>
     <div id={styles.background1}>
+    <Header />
         <div id={styles.background2}>
+        
         
     {
         // this iterates through the articles JSON and
@@ -28,6 +30,7 @@ const ArticleList = ({ articles }) => (
 
      
       </div>
+      <Footer />
     </div>
     </section>  
   );
